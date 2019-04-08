00dd2ed9eb39b272b2cb4595ae8e96c1c65fee01
Pawel Mysior <pawelmysior@gmail.com>
2019-04-08 20:13:51 +0200

    Add svg-icon Vue component

---
+++ resources/js/components/SvgIcon.vue
@@ -0,0 +1,14 @@
+<template>
+    <div v-html="require('!!html-loader!./../../svg/zondicons/' + this.icon + '.svg')" v-once></div>
+</template>
+
+<script>
+  export default {
+    props: ['icon'],
+    mounted() {
+      this.$el.firstChild.classList.add(...this.$el.className.split(' '));
+      this.$el.firstChild.classList.add('fill-current');
+      this.$el.outerHTML = this.$el.innerHTML;
+    },
+  };
+</script>
