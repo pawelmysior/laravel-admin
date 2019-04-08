53317dee515a3231d1f5f3d25f13a155666415f9
Pawel Mysior <pawelmysior@gmail.com>
2019-04-08 20:40:19 +0200

    Add menu

---
+++ resources/js/components/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <a class="flex items-center mb-3 text-sm text-white hover:text-blue-400" :href="route" v-once>
+        <svg-icon class="text-blue-400 h-4 w-4 mr-3" :icon="icon"></svg-icon>
+        <span v-html="label"></span>
+    </a>
+</template>
+
+<script>
+  export default {
+    props: [
+      'icon',
+      'label',
+      'route',
+    ],
+  };
+</script>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,9 +11,36 @@
         <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i" rel="stylesheet">
         <link href="{{ asset(mix('app.css', 'vendor/admin')) }}" rel="stylesheet">
     </head>
-    <body class="font-sans">
+    <body class="bg-gray-300 text-gray-900 font-sans">
         <div id="app">
-            @yield('content')
+            <div class="flex min-h-screen">
+                <div class="bg-blue-800">
+                    <div class="h-full w-48 py-6 px-5 flex flex-col content-between">
+                        <div class="flex-grow">
+                            @include('admin.menu')
+                        </div>
+
+                        <div>
+                            <a
+                                class="flex items-center text-sm text-white hover:text-blue-400"
+                                href="{{ route('logout') }}"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                            >
+                                <svg-icon icon="close-outline" class="text-blue-400 h-4 w-4 mr-3"></svg-icon>
+                                <span>Wyloguj się</span>
+                            </a>
+
+                            <form id="logout-form" class="hidden" action="{{ route('logout') }}" method="POST">
+                                @csrf
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex-grow p-10">
+                    @yield('content')
+                </div>
+            </div>
         </div>
 
         <!-- Scripts -->
