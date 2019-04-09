276deb564a479515ab14ac54e30e9b14c2327a66
Pawel Mysior <pawelmysior@gmail.com>
2019-04-09 18:53:41 +0200

    Add create-button and edit-button components

---
+++ resources/js/components/CreateButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <a class="btn btn-low btn-blue" :href="route">
+        <slot>Nowy</slot>
+    </a>
+</template>
+
+<script>
+  export default {
+    props: ['route'],
+  };
+</script>
---
+++ resources/js/components/EditButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <a class="btn btn-low btn-blue-outline" :href="route">
+        <slot>Edytuj</slot>
+    </a>
+</template>
+
+<script>
+  export default {
+    props: ['route'],
+  };
+</script>
