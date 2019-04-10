e0382bcf442b3e19f8366bf94a8a9a493c294006
Pawel Mysior <pawelmysior@gmail.com>
2019-04-10 12:47:26 +0200

    Add the dictionary() helper

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,16 @@
 <?php
 
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
 function attribute(string $name): string
 {
     return ucfirst(__("validation.attributes.{$name}"));
+}
+
+function dictionary(string $name, string $valueColumn = 'name', string $keyColumn = 'id'): Collection
+{
+    $modelClass = 'App\\' . Str::studly(Str::singular($name));
+
+    return $modelClass::get()->pluck($valueColumn, $keyColumn);
 }
