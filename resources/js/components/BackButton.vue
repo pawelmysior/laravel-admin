d28354cf5811611304a58491324f0be2de97222e
Pawel Mysior <pawelmysior@gmail.com>
2019-04-10 11:46:13 +0200

    Add back-button component

---
+++ resources/js/components/BackButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <a class="btn btn-low btn-gray" :href="route">
+        <slot>Wróć do listy</slot>
+    </a>
+</template>
+
+<script>
+  export default {
+    props: ['route'],
+  };
+</script>
