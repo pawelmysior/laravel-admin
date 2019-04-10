50d6e4d77a89859a948bb6f9f7e1c01e5996f060
Pawel Mysior <pawelmysior@gmail.com>
2019-04-10 16:37:59 +0200

    Add password-input component

---
+++ resources/js/components/inputs/PasswordInput.vue
@@ -0,0 +1,38 @@
+<template>
+    <form-input
+        :label="label"
+        :labelExtra="labelExtra"
+        :name="name"
+        :required="required"
+        :value="value"
+        v-once
+    >
+        <input
+            slot-scope="{ error, old }"
+            class="input"
+            :class="{ 'has-error': error }"
+            :id="name"
+            :name="name"
+            type="password"
+            :value="old"
+            autocomplete="new-password"
+        >
+    </form-input>
+</template>
+
+<script>
+  import FormInput from './FormInput';
+
+  export default {
+    components: {
+      FormInput,
+    },
+    props: [
+      'label',
+      'labelExtra',
+      'name',
+      'required',
+      'value',
+    ],
+  };
+</script>
