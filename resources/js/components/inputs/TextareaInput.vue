128f29e0c89dee678c78bf6f62b48cbea27201a2
Pawel Mysior <pawelmysior@gmail.com>
2019-04-10 12:46:51 +0200

    Add textarea-input component

---
+++ resources/js/components/inputs/TextareaInput.vue
@@ -0,0 +1,37 @@
+<template>
+    <form-input
+        :label="label"
+        :labelExtra="labelExtra"
+        :name="name"
+        :required="required"
+        :value="value"
+        v-once
+    >
+        <textarea
+            slot-scope="{ error, old }"
+            class="input"
+            :class="{ 'has-error': error }"
+            :id="name"
+            :name="name"
+            :rows="rows ? rows : 5"
+        >{{ old }}</textarea>
+    </form-input>
+</template>
+
+<script>
+  import FormInput from './FormInput';
+
+  export default {
+    components: {
+      FormInput,
+    },
+    props: [
+      'label',
+      'labelExtra',
+      'name',
+      'required',
+      'value',
+      'rows',
+    ],
+  };
+</script>
