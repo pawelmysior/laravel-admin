bfbb5ecb3adc6a3d498eef813c7760ebc8c3baed
Pawel Mysior <pawelmysior@gmail.com>
2019-07-16 18:40:44 +0200

    Improve the layout on small screens

--- resources/js/components/MenuItem.vue
+++ resources/js/components/MenuItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <a class="flex items-center mb-3 text-sm text-white hover:text-blue-400" :href="route" v-once>
+    <a class="flex items-center mb-3 whitespace-no-wrap text-sm text-white hover:text-blue-400" :href="route" v-once>
         <svg-icon class="text-blue-400 h-4 w-4 mr-3" :icon="icon"></svg-icon>
         <span v-html="label"></span>
     </a>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,31 +23,42 @@
     </head>
     <body class="bg-gray-300 text-gray-900 font-sans">
         <div id="app" v-cloak>
-            <div class="flex min-h-screen">
-                <div class="bg-blue-800">
-                    <div class="h-full w-48 py-6 px-5 flex flex-col content-between">
-                        <div class="flex-grow">
-                            @include('admin.menu')
-                        </div>
+            <div class="min-h-screen flex flex-col xl:flex-row items-stretch">
+                <renderless-toggler>
+                    <div class="bg-blue-800 flex flex-col" slot-scope="{ hidden, toggle }">
+                        <button class="self-end text-white flex xl:hidden items-center px-5 py-5 appearance-none focus:outline-none" @click="toggle">
+                            <svg class="fill-current h-3 w-3" :class="{ hidden: hidden }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+                            </svg>
+                            <svg class="fill-current h-3 w-3" :class="{ hidden: !hidden }" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
+                            </svg>
+                        </button>
 
-                        <div>
-                            <a
-                                class="flex items-center text-sm text-white hover:text-blue-400"
-                                href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                            >
-                                <svg-icon icon="close-outline" class="text-blue-400 h-4 w-4 mr-3"></svg-icon>
-                                <span>Wyloguj się</span>
-                            </a>
+                        <div class="xl:flex flex-col h-full justify-between pb-6 xl:pt-6 px-5 xl:pr-20" :class="{ hidden: !hidden }">
+                            <div class="flex-grow">
+                                @include('admin.menu')
+                            </div>
 
-                            <form id="logout-form" class="hidden" action="{{ route('logout') }}" method="POST">
-                                @csrf
-                            </form>
+                            <div>
+                                <a
+                                    class="flex items-center whitespace-no-wrap text-sm text-white hover:text-blue-400"
+                                    href="{{ route('logout') }}"
+                                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                                >
+                                    <svg-icon icon="close-outline" class="text-blue-400 h-4 w-4 mr-3"></svg-icon>
+                                    <span>Wyloguj się</span>
+                                </a>
+
+                                <form id="logout-form" class="hidden" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </renderless-toggler>
 
-                <div class="flex-grow p-10">
+                <div class="flex-grow p-4 xl:p-10">
                     @yield('content')
                 </div>
             </div>
